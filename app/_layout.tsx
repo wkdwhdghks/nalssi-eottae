0130e023fa9dd9c286c27cbd7c6b00186bcf9efb
jangjonghwan <wkdwhdghks97@gmail.com>
2025-04-16 21:57:42 +0900

    feat: SUIT 폰트 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import { useFonts } from 'expo-font';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    'SUIT-400': require('@/assets/fonts/SUIT-Regular.ttf'),
+    'SUIT-500': require('@/assets/fonts/SUIT-Medium.ttf'),
+    'SUIT-600': require('@/assets/fonts/SUIT-SemiBold.ttf'),
+    'SUIT-700': require('@/assets/fonts/SUIT-Bold.ttf'),
+    'SUIT-800': require('@/assets/fonts/SUIT-ExtraBold.ttf'),
+  });
+
+  if (!loaded) return null;
+
   return (
-    <View>
-      <Text>테스트입니다.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text style={{ fontFamily: 'SUIT-700', fontSize: 20 }}>테스트</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
